136298a55ffc455810ecfb66fdc86cf51e5b75a0
el-schneider <mail@jonaslist.de>
2025-12-16 20:47:56 +0100

    refactor: extend ProvideMagicActionsToScript to handle additional event types including Term, Global Variables, and User blueprints

--- src/Listeners/ProvideMagicActionsToScript.php
+++ src/Listeners/ProvideMagicActionsToScript.php
@@ -7,14 +7,18 @@ namespace ElSchneider\StatamicMagicActions\Listeners;
 use ElSchneider\StatamicMagicActions\Services\MagicFieldsConfigBuilder;
 use Statamic\Events\AssetContainerBlueprintFound;
 use Statamic\Events\EntryBlueprintFound;
+use Statamic\Events\GlobalVariablesBlueprintFound;
+use Statamic\Events\TermBlueprintFound;
+use Statamic\Events\UserBlueprintFound;
 use Statamic\Statamic;
 
 final class ProvideMagicActionsToScript
 {
     public function __construct(private MagicFieldsConfigBuilder $configBuilder) {}
 
-    public function handle(EntryBlueprintFound|AssetContainerBlueprintFound $event): void
-    {
+    public function handle(
+        EntryBlueprintFound|AssetContainerBlueprintFound|TermBlueprintFound|GlobalVariablesBlueprintFound|UserBlueprintFound $event
+    ): void {
         if (! $this->isMatchingRoute($event)) {
             return;
         }
@@ -26,8 +30,9 @@ final class ProvideMagicActionsToScript
         ]);
     }
 
-    private function isMatchingRoute(EntryBlueprintFound|AssetContainerBlueprintFound $event): bool
-    {
+    private function isMatchingRoute(
+        EntryBlueprintFound|AssetContainerBlueprintFound|TermBlueprintFound|GlobalVariablesBlueprintFound|UserBlueprintFound $event
+    ): bool {
         $routeName = request()->route()?->getName();
 
         if (! $routeName) {
@@ -37,6 +42,9 @@ final class ProvideMagicActionsToScript
         $expectedPrefix = match (true) {
             $event instanceof EntryBlueprintFound => 'statamic.cp.collections',
             $event instanceof AssetContainerBlueprintFound => 'statamic.cp.assets',
+            $event instanceof TermBlueprintFound => 'statamic.cp.taxonomies',
+            $event instanceof GlobalVariablesBlueprintFound => 'statamic.cp.globals',
+            $event instanceof UserBlueprintFound => 'statamic.cp.users',
         };
 
         return str_starts_with($routeName, $expectedPrefix);
