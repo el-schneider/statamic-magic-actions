e9821d398d31ab5675013c97e95507f062f3b951
Ito <ito@soft-lifes.com>
2026-02-16 01:42:39 +0100

    test: add Pest browser tests for field actions and settings page

--- tests/Pest.php
+++ tests/Pest.php
@@ -16,6 +16,9 @@ declare(strict_types=1);
 pest()->extend(Tests\TestCase::class)
     ->in('Unit', 'Feature');
 
+pest()->extend(Tests\BrowserTestCase::class)
+    ->in('Browser');
+
 /*
 |--------------------------------------------------------------------------
 | Expectations
